7d84dd8f17c12c1f2fedce46b79427384221b400
Léo Garret <leodu34@hotmail.com>
2018-12-28 11:46:24 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,77 @@
+$(document).ready(function(){
+
+  var $mainMenuItems = $("#main-menu ul").children("li");
+  var totalMainMenuItems = $mainMenuItems.length;
+  var openedIndex = 2;
+  var init = function(){
+
+    bindEvents();
+
+    if(validIndex(openedIndex))
+    {
+      animateItem($mainMenuItems.eq(openedIndex), true, 700);
+    }
+
+  };
+
+  var bindEvents = function(){
+
+    $mainMenuItems.children(".images").click(function(){
+
+      var newIndex = $(this).parent().index();
+      checkAnimateItem(newIndex);
+    });
+
+    $(".button").hover(function(){
+      $(this).addClass("hovered");
+    },
+    function(){
+      $(this).removeClass("hovered");
+    });
+
+    $(".button").click(function(){
+      var newIndex = $(this).index();
+      checkAnimateItem(newIndex);
+    })
+
+  };
+
+  var validIndex = function(indexToCheck){
+
+    return (indexToCheck >= 0) && (indexToCheck < totalMainMenuItems);
+
+  };
+
+  var animateItem = function($item, toOpen, speed){
+
+    var $colorImage = $item.find(".color");
+    var itemParam = toOpen ? {width: "420px"}:{width: "140px"};
+    var colorImageParam = toOpen ? {left: "0px"}:{left: "140px"};
+
+    $colorImage.animate(colorImageParam, speed);
+    $item.animate(itemParam, speed);
+
+  };
+
+  var checkAnimateItem = function(indexToCheckAndAnimate){
+
+    if(openedIndex == indexToCheckAndAnimate)
+    {
+      animateItem($mainMenuItems.eq(indexToCheckAndAnimate), false, 250);
+      openedIndex = -1;
+    }
+    else
+    {
+      if(validIndex(indexToCheckAndAnimate))
+      {
+        animateItem($mainMenuItems.eq(openedIndex), false, 250);
+        openedIndex = indexToCheckAndAnimate;
+        animateItem($mainMenuItems.eq(openedIndex), true, 250);
+      }
+    }
+
+  };
+
+  init();
+
+});
